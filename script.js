541b152a5a4fcb3cf7716c5bc91ecdeaddd32ef3
krerenchin <101045044+krerenchin@users.noreply.github.com>
2025-02-23 23:04:15 +0900

    Review steps 2-5, add comments to functions

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 var humanScore = 0
 var computerScore = 0
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();  
 
 function getComputerChoice() {
     const compChoice = Math.floor(Math.random () * 3) + 1;  
     if (compChoice === 1) return "rock";
     else if (compChoice === 2) return "paper";
-    else return "scissors";
+    else return "scissors";  
 }
-console.log(getComputerChoice())
-
-
+console.log("ComputerSelectionReturn", computerSelection); //show compChoice as a string
 
 function getHumanChoice() {
-    let humChoice = prompt("Choose rock, paper or scissors and type it here");
+    let humChoice = prompt("Choose rock, paper or scissors and type it here", "rock"); //after comma - make default input
+    console.log("Human choice:", humChoice); //show input from prompt BUT not the result of return
     let regex1 = /rock/i;
     let regex2 = /paper/i;
     let regex3 = /scissors/i;
@@ -24,57 +23,53 @@ function getHumanChoice() {
     else if (humChoice.match(regex2)) return "paper";
     else if (humChoice.match(regex3)) return "scissors";
     else {
-        alert("Please type correctly");
+        alert("Please type correctly"); //make it show prompt again 
+    // getHumanChoice(); Get error, no return
     }
-
 }
-console.log(getHumanChoice()) 
-// need to fix showing of this function in console 
+console.log("HumanSelectionReturn", humanSelection);
 
-function playRound(humChoice, compChoice) {
-    if (compChoice === "rock")
-     if (humChoice === "paper")
-      console.log("You won")
 
-    if (compChoice === "scissors")
-     if (humChoice === "paper")
-      console.log("I won")
+function playRound(computerSelection, humanSelection) {
+if (computerSelection === humanSelection) {
+    console.log("It's a draw");
+} else if (computerSelection === "rock" && humanSelection === "paper") {
+    humanScore = humanScore + 1; //counter for score
+    console.log("Hum won, pap beats rock");
+} else if (computerSelection === "rock" && humanSelection === "scissors") {
+    computerScore = computerScore + 1;
+    console.log("Comp won, rock beats sc");
+} else if (computerSelection === "paper" && humanSelection === "rock") {
+    computerScore = computerScore + 1;
+    console.log("Comp won, pap beats rock");
+} else if (computerSelection === "paper" && humanSelection === "scissors") {
+    humanScore = humanScore + 1;
+    console.log("Hum won, sc beats pap");
+} else if (computerSelection === "scissors" && humanSelection === "rock") {
+    humanScore = humanScore + 1;
+    console.log("Hum won, rock beats sc");
+} else if (computerSelection === "scissors" && humanSelection === "paper") {
+    computerScore = computerScore + 1;
+    console.log("Comp won, sc beats pap");
+}
+} 
+playRound(computerSelection, humanSelection); //Who won message. Add prompt in play Round, to start new round.
 
-    if (compChoice === "paper")
-        if (humChoice === "paper")
-         console.log("It's draw")
+playRound(computerSelection, humanSelection);
 
-    if (compChoice === "rock")
-        if (humChoice === "rock")
-         console.log("It's a draw")
+console.log();
 
-    if (compChoice === "scissors")
-        if (humChoice === "rock")
-         console.log("You won")
+console.log("ComputerScore:", computerScore, "HumanScore:", humanScore);
 
-    if (compChoice === "paper")
-        if (humChoice === "rock")
-         console.log("I won")
-
-    if (compChoice === "scissors")
-        if (humChoice === "scissors")
-         console.log("It's a draw")
-
-    if (compChoice === "rock")
-        if (humChoice === "scissors")
-         console.log("I won")
+// function playGame () {
     
-    if (compChoice === "paper")
-        if (humChoice === "scissors")
-         console.log("You won")
+//     let round = //playgame???;
 
-}
-  
-  playRound(humanSelection, computerSelection);
-  
+// //     for (let i = 1; i <= 5; i++) {
+// //   console.log(i);
+// //     } make a loop for 5 rounds
 
-
-
+// }
 
 
 
